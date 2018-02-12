9317ba6ff1a125096ed9fccddf6645b30d579c34
omnifoodtogo <omnifoodtogo@gmail.com>
2018-02-11 23:30:55 -0600

    added gmaps

--- resources/js/script.js
+++ resources/js/script.js
@@ -107,4 +107,22 @@ $(function(){
         }
     });
     
+    /* Maps */
+    
+    var map = new GMaps({
+  div: '.map',
+  lat: 38.7436057,
+  lng: -9.05,
+  zoom:12
+});
+    
+    map.addMarker({
+  lat: 38.7436057,
+  lng: -9.2302434,
+  title: 'Lisbon',
+infoWindow: {
+  content: '<p>Our Lisbon head quarters</p>'
+}
+});
+    
 });
